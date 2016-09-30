3fac5b54650c6a9ff3de24fe0ca1078cc2c499e6
Philhawk <pkjacob@gmail.com>
2016-09-30 16:09:40 -0400

    up to date

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,14 @@ var Restaurant = models.Restaurant;
 var Activity = models.Activity;
 var Promise = require('bluebird')
 
-exports.home = function(req, res) {
-  res.render('home');
+exports.home = function(req, res, next) {
+  Hotel.findAll({})
+  .then(function(eachHotel){
+    var hotels = eachHotel.map(hotel => hotel.dataValues)
+    console.log('THESE ARE THE HOTELS', hotels)
+    res.render('home', { hotels: hotels });
+  })
+  .catch(function(err){
+    console.err
+  })
 }
